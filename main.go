894cf11a70eb4c7598f05e0c784fa5d4a89634f5
A. Schulze <git@andeasschulze.de>
2025-07-13 10:45:17 +0200

    meine updates

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -31,6 +32,7 @@ var (
 	serveHTTP         = flag.Bool("http", false, "If true, serves HTTP instead of HTTPS (and does not fetch certs). Useful for serving behind an HTTPS-terminating proxy.")
 	staging           = flag.Bool("staging", false, "If true, uses Let's Encrypt 'staging' environment instead of prod.")
 	acmeEndpoint      = flag.String("acme_endpoint", "", "If set, uses a custom ACME endpoint URL. It doesn't make sense to use this with --staging.")
+	version           = flag.Bool("version", false, "print version and exit.")
 
 	// Policy options.
 	mirrorStsFrom = flag.String("mirror_sts_from", "", "If set (e.g. 'google.com'), proxy the STS policy for this domain.")
@@ -82,6 +84,10 @@ func hostPolicy() autocert.HostPolicy {
 
 func main() {
 	flag.Parse()
+	if *version {
+		fmt.Printf("sts-mate, built with %s\n", runtime.Version())
+		os.Exit(0)
+	}
 	if *domains == "" && *myRealHost == "" && !*serveHTTP {
 		// Note that if we are serving HTTP, --domain and --my_real_host do nothing.
 		fmt.Fprintln(os.Stderr, "Must specify --domain or --my_real_host for safety.")
@@ -100,10 +106,12 @@ func main() {
 		os.Exit(2)
 	}
 	if *serveHTTP && (*staging || (*acmeEndpoint != "")) ||
-	 (*staging && (*acmeEndpoint != "")) {
+		(*staging && (*acmeEndpoint != "")) {
 		fmt.Fprintln(os.Stderr, "Only one of --http, --staging, and --acme_endpoint can be used.")
 		os.Exit(2)
 	}
+
+	fmt.Printf("starting sts-mate, built with %s\n", runtime.Version())
 
 	// Serve policies.
 	var stsMirror string
@@ -161,6 +169,18 @@ func main() {
 		srv.Addr = ":https"
 		srv.TLSConfig = cm.TLSConfig()
 		srv.TLSConfig.MinVersion = tls.VersionTLS12
+		srv.TLSConfig.PreferServerCipherSuites = true
+		srv.TLSConfig.CipherSuites = []uint16{
+			// same as 'openssl ciphers -v "ECDHE+AESGCM:ECDHE+CHACHA20"'
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		}
+		// disable http2 and higher
+		srv.TLSConfig.NextProtos = []string{"http/1.0", "http/1.1", "acme-tls/1"}
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
